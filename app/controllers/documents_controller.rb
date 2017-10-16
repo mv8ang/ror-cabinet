18ca902a97f74637cbb3231566db2f9778162816
mv8ang <xfk.dgt@gmail.com>
2017-10-16 12:08:05 +0200

    add documents controller and routes

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,32 @@
+class DocumentsController < ApplicationController
+
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+  
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def find_document
+    end
+
+    def document_params
+    end
+  
+end
