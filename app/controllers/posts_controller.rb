b65ab48ecdd8f75c75ae85b4dc98c8271073df6e
mv8ang <xfk.dgt@gmail.com>
2017-10-17 11:38:23 +0200

    change index to show current user posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all.order("created_at DESC")
+    @posts = Post.where(user_id: current_user)
   end
   
   def show
